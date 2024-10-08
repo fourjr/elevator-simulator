c2c35f87840769a15f69b042af76f5c158e27eaf
Yee Jia Rong <28086837+fourjr@users.noreply.github.com>
2024-10-09 01:02:50 +0800

    remove listener in elevatorpanel

--- web/frontend/src/components/ElevatorPanel.tsx
+++ web/frontend/src/components/ElevatorPanel.tsx
@@ -5,18 +5,6 @@ import { Box, Typography } from "@mui/material";
 import { useEffect } from "react";
 
 export default function ElevatorPanel({elevators}: {elevators: Elevator[]}) {
-    useEffect(() => {
-        document.addEventListener('wsMessage', (event) => {
-            const message = (event as WSEvent).message;
-            if (message.command === ServerCommand.ADD_ELEVATOR) {
-                console.log('add elevator');
-            }
-            else if (message.command === ServerCommand.REMOVE_ELEVATOR) {
-                console.log('remove elevator');
-            }
-        })
-    }, [])
-
     return <Box>{
         elevators.map(elevator => 
             <Typography>Elevator {elevator.id} at floor {elevator.currentFloor}{elevator.destinationFloor === null ? "" : `, going to ${elevator.destinationFloor}`}</Typography>
